2a9e0ce7e6d11b6b62a20b22d9bd784e06d3908c
Bellil <ikram.24032004@gmail.com>
2026-04-19 01:56:33 +0100

    fix render crash

--- frontend/config.js
+++ frontend/config.js
@@ -21,8 +21,6 @@
  */
 (function () {
     const SUPABASE_URL = "https://castbrkpjiyfmtbrlccd.supabase.co";
-
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNhc3Ricmtwaml5Zm10YnJsY2NkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1MzQ5MDksImV4cCI6MjA5MjExMDkwOX0.JTDCiNJD8JPaEGpJ5cH9j11k9uJetw5UtHUt0JMUU2o";
-
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+    const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNhc3Ricmtwaml5Zm10YnJsY2NkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1MzQ5MDksImV4cCI6MjA5MjExMDkwOX0.JTDCiNJD8JPaEGpJ5cH9j11k9uJetw5UtHUt0JMUU2o";
+    window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 })();
